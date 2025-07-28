5d0204f353d6f9d39a7af29ad3acba0435c31e27
EasyToGet <yide1986@gmail.com>
2025-07-29 01:52:20 +0800

    fix: 修改 githug pages 網址路徑

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/vite-init2025/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
